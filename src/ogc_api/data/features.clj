d473e49abafc6de0d209b2be55eb8302834f53f7
Kieran Livesey <kieran@swirrl.com>
2021-12-15 09:55:22 +0000

    Filter bounding box (#2)

--- src/ogc_api/data/features.clj
+++ src/ogc_api/data/features.clj
@@ -60,7 +60,10 @@
     (into []
           (sparql/query
             query-path
-            (cond-> {:collection (java.net.URI. collection-uri)}
+            (cond-> {:collection (java.net.URI. collection-uri)
+                     :filter_bbox (Boolean. (some? bbox))}
+              (some? bbox) (assoc :bbox_lat1 (bbox 0) :bbox_lon1 (bbox 1)
+                                  :bbox_lat2 (bbox 2) :bbox_lon2 (bbox 3))
               (some? feature-id) (assoc :id feature-id)
               (some? offset) (assoc ::sparql/offsets {0 offset})
               (some? limit) (assoc ::sparql/limits {10 limit}))
